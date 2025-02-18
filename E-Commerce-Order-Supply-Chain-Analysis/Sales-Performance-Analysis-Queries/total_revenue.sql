ba05e87c72d3f73fcbcdd9433dcc6441a026da27
Ralph Gerard Dela Cruz <imperialayase@gmail.com>
2025-02-19 00:46:53 +0800

    Update total_revenue.sql

--- E-Commerce-Order-Supply-Chain-Analysis/Sales-Performance-Analysis-Queries/total_revenue.sql
+++ E-Commerce-Order-Supply-Chain-Analysis/Sales-Performance-Analysis-Queries/total_revenue.sql
@@ -1,1 +1,5 @@
-
+-- Find the total revenue and numbers of orders in the dataset:
+SELECT 
+    COUNT(DISTINCT oi.order_id) AS total_orders,
+    SUM(oi.price + oi.shipping_charges) AS total_revenue
+FROM order_items oi;
