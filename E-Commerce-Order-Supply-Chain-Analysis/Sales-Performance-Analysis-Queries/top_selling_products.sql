afa27debe1405244d6318c6b438a32b385400b9c
Ralph Gerard Dela Cruz <imperialayase@gmail.com>
2025-02-19 00:47:52 +0800

    Update top_selling_products.sql

--- E-Commerce-Order-Supply-Chain-Analysis/Sales-Performance-Analysis-Queries/top_selling_products.sql
+++ E-Commerce-Order-Supply-Chain-Analysis/Sales-Performance-Analysis-Queries/top_selling_products.sql
@@ -1,1 +1,11 @@
-
+--Find the top 5 best selling products based on revenue:
+SELECT 
+    oi.product_id, 
+    p.product_category_name, 
+    COUNT(oi.order_id) AS total_orders, 
+    SUM(oi.price) AS total_revenue
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY oi.product_id, p.product_category_name
+ORDER BY total_revenue DESC
+LIMIT 5;
